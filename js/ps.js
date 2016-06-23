7a4face53cad5c17b8b836a0db300d8ee8ad2d32
Boudhayan Banerjee <boudhayanbanerjee@gmail.com>
2016-06-22 22:49:04 -0500

    Update ps.js

--- js/ps.js
+++ js/ps.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 		$("#ps").click(function(){
+			$("#wrapper").toggleClass("toggled");
 			$.getJSON(url_ps,function(data){
 				if(data){
 					$.each(data,function(key,value){
